3586449f0e598359f27682fa1ec35f8c6d1e837b
Shreepad Patil <sp1467@null.cs.rutgers.edu>
2017-05-06 03:55:30 -0400

    Added algorithm - almost there

--- multNoShare.cu
+++ multNoShare.cu
@@ -60,12 +60,12 @@ __global__ void ChainMatMulKernel(Matrix* Chain, int* Muls, Matrix* IntRes) {
 }
 
 
-Matrix ChainMatMul(Matrix* Chain, int n) {
+Matrix ChainMatMulHC(Matrix* Chain, int n) {
 
 	Matrix Result;
 	Matrix* h_Chain; // Elements on device
 	Matrix* d_Chain; // Array fully on device
-	Matrix* IntRes; // Array fully on host
+	// Matrix* IntRes; // Array fully on host
 	Matrix* h_IntRes; // Elements on device
 	Matrix* d_IntRes; // Array fully on device
 
@@ -318,6 +318,236 @@ Matrix ChainMatMul(Matrix* Chain, int n) {
 	return Result;
 }
 
+void SequentialSelectionSortDouble(int* array, int* arrayOrder, int n) {
+	int position, swap, swapOrder, d, c;
+
+	for(c = 0 ; c < ( n - 1 ) ; c++) {
+		position = c;
+ 
+      	for ( d = c + 1 ; d < n ; d++ ) {
+        	if ( array[position] < array[d] )
+            	position = d;
+      	}
+      	if(position != c) {
+        	swap = array[c];
+        	swapOrder = arrayOrder[c];
+         
+        	array[c] = array[position];
+        	arrayOrder[c] = arrayOrder[position];
+
+        	array[position] = swap;
+        	arrayOrder[position] = swapOrder;
+      	}
+   	}
+}
+
+void SequentialSelectionSort(int* array, int n) {
+	int position, swap, d, c;
+
+	for(c = 0 ; c < ( n - 1 ) ; c++) {
+		position = c;
+ 
+      	for ( d = c + 1 ; d < n ; d++ ) {
+        	if ( array[position] > array[d] )
+            	position = d;
+      	}
+      	if(position != c) {
+        	swap = array[c];
+         
+        	array[c] = array[position];
+
+        	array[position] = swap;
+      	}
+   	}
+}
+
+Matrix ChainMatMul(Matrix* Chain, int numMats) {
+
+	int n = numMats;
+	Matrix Result;
+	Matrix* h_Chain; // Only elements on device
+	Matrix* d_Chain; // Array fully on device
+	Matrix* h_IntRes; // Only elements on device
+	Matrix* d_IntRes; // Array fully on device
+	int* ChainDims;
+	int* ChainDimOrder;
+	int numDims;
+	int* h_muls; // Array on host
+	int* d_muls; // Array on device
+	int numMuls = 0;
+	
+	h_Chain = (Matrix*)malloc(n*sizeof(Matrix));
+	
+	size_t size;
+	cudaError_t err;
+	
+	// Transfer from Chain to h_Chain
+	for(int i = 0; i < n;++i) {	        
+		h_Chain[i].width = Chain[i].width;
+		h_Chain[i].height = Chain[i].height;
+		size = h_Chain[i].width * h_Chain[i].height * sizeof(float);
+		err = cudaMalloc(&h_Chain[i].elements, size);
+		printf("CUDA malloc Chain[%d].elements: %s\n", i, cudaGetErrorString(err));
+		err = cudaMemcpy(h_Chain[i].elements, Chain[i].elements, size, cudaMemcpyHostToDevice);
+		printf("Copy Chain[%d].elements to device: %s\n", i, cudaGetErrorString(err));
+	}
+		
+	// Trasfer from h_Chain to d_Chain
+	size = n * sizeof(Matrix);
+	err = cudaMalloc(&d_Chain, size);
+	printf("CUDA malloc Chain: %s\n", cudaGetErrorString(err));
+	err = cudaMemcpy(d_Chain, h_Chain, size, cudaMemcpyHostToDevice);
+	printf("Copy Chain to device: %s\n", cudaGetErrorString(err));
+	
+
+	while (n > 1) {
+		// ************************** Find optimal multiplications ******************
+		// Fill up ChainDims
+		numDims = n - 1;
+		numMuls = 0;
+		ChainDims = (int*)malloc(numDims * sizeof(int));
+		ChainDimOrder = (int*)malloc(numDims * sizeof(int));
+		h_muls = (int*)malloc(numDims * sizeof(int));
+		for(int i = 0; i < numDims; ++i) {
+			ChainDims[i] = h_Chain[i].width;
+			ChainDimOrder[i] = i;
+		}
+
+		// Sort ChainDims
+		SequentialSelectionSortDouble(ChainDims, ChainDimOrder, numDims);
+
+		// Select muls
+		for(int i = 0, j = 0;i < numDims; ++i) {
+			if(ChainDims[i] != 0) {
+				h_muls[j] = ChainDimOrder[i];
+				numMuls++;
+				j++;
+				for(int k = 0; k < numDims; k++){
+					if(ChainDimOrder[k] == (ChainDimOrder[i] + 1) || ChainDimOrder[k] == (ChainDimOrder[i] - 1)) {
+						ChainDims[k] = 0;
+					}
+				}
+			}
+		}
+		free(ChainDims);
+		free(ChainDimOrder);
+
+		SequentialSelectionSort(h_muls, numMuls);
+		printf("\nh_muls : ");
+		for(int i = 0; i < numMuls; ++i) {
+			printf("%d ", h_muls[i]);
+		}
+		// **************************************************************************
+
+		// ********************** Transfer stuff to Device **************************
+		// Transfer muls on device
+		err = cudaMalloc(&d_muls, numMuls * sizeof(int));
+		printf("CUDA malloc Muls: %s\n", cudaGetErrorString(err));
+		err = cudaMemcpy(d_muls, h_muls, numMuls * sizeof(int), cudaMemcpyHostToDevice);
+		printf("Copy Muls to device: %s\n", cudaGetErrorString(err));
+
+		// Hold intermediate results on host with elements on device
+		h_IntRes = (Matrix*)malloc(numMuls * sizeof(Matrix));
+		
+		// Allocate memory on device for the elements of h_IntRes
+		for(int i = 0; i < numMuls; ++i) {
+			h_IntRes[i].height = h_Chain[h_muls[i]].height;
+			h_IntRes[i].width = h_Chain[h_muls[i] + 1].width;
+			size_t size = h_IntRes[i].width * h_IntRes[i].height * sizeof(float);
+			err = cudaMalloc(&h_IntRes[i].elements, size);
+			printf("CUDA malloc IntRes[%d]: %s\n", i, cudaGetErrorString(err));
+		}
+		
+		// IntRes Fully on device
+		size = numMuls * sizeof(Matrix);
+		err = cudaMalloc(&d_IntRes, size);
+		printf("CUDA malloc Chain: %s\n", cudaGetErrorString(err));
+		err = cudaMemcpy(d_IntRes, h_IntRes, size, cudaMemcpyHostToDevice);
+		printf("Copy Chain to device: %s\n", cudaGetErrorString(err));
+		
+		// **************************************************************************
+
+		// *************************** Actual Multiplication ************************
+		dim3 dimGrid(numMuls);
+
+		// Call to the kernel
+		ChainMatMulKernel<<<1, dimGrid>>>(d_Chain, d_muls, d_IntRes); 
+		err = cudaThreadSynchronize();
+		printf("Run kernel: %s\n", cudaGetErrorString(err));
+
+		// **************************************************************************
+
+		// ************************** Readying for next cycle ***********************
+		// Update chain 
+		for(int i = 0; i < numMuls;++i) {
+			// Free device memory
+			cudaFree(h_Chain[h_muls[i]].elements);
+			cudaFree(h_Chain[h_muls[i] + 1].elements);
+			
+			// Update the chain
+			h_Chain[h_muls[i]].height = h_IntRes[i].height;
+			h_Chain[h_muls[i]].width = h_IntRes[i].width;
+			h_Chain[h_muls[i]].elements = h_IntRes[i].elements;
+		}
+		
+		// Reduce the size of the h_Chain array
+		int i , j;
+		for(i = 0, j = 0; i < n;) {
+			printf("Iter : %d,%d,%d\n", i,j,n);
+			
+			//h_muls[i] is the error
+			while(h_muls[j] > i) {
+				h_Chain[i].width = h_Chain[i + j].width;	
+				h_Chain[i].height = h_Chain[i + j].height;
+				h_Chain[i].elements = h_Chain[i + j].elements;
+				i++;
+			}
+			i++;
+			++j;
+			--n;
+		}
+		// Small memory leak here - should use Free h_Chain.elements for last few elements(but removing this is difficult)
+
+		// Refresh d_Chain
+		cudaFree(d_Chain);
+		
+		size = n * sizeof(Matrix);
+		err = cudaMalloc(&d_Chain, size);
+		printf("CUDA malloc Chain: %s\n", cudaGetErrorString(err));
+		err = cudaMemcpy(d_Chain, h_Chain, size, cudaMemcpyHostToDevice);
+		printf("Copy Chain to device: %s\n", cudaGetErrorString(err));
+			
+		// Free stuff
+		free(h_muls);
+		cudaFree(d_muls);
+		free(h_IntRes);
+		cudaFree(d_IntRes);
+
+		// **************************************************************************
+	}
+	
+	// Read Result from device memory
+	Result.width = h_Chain[0].width;	
+	Result.height = h_Chain[0].height;
+	size = Result.width * Result.height * sizeof(float);
+	Result.elements =  (float*)malloc(size);
+	printf("\n Result : %d x %d : ", Result.height, Result.width);
+	err = cudaMemcpy(Result.elements, h_Chain[0].elements, size, cudaMemcpyDeviceToHost);
+	printf("Copy Result off of device: %s\n",cudaGetErrorString(err));
+	
+	
+	// Free device memory
+	for(int i = 0; i < numMats;++i) {        
+		cudaFree(h_Chain[i].elements);
+	}
+	
+	cudaFree(d_muls);
+	cudaFree(d_Chain);
+	free(h_muls);
+	free(h_Chain);
+	
+	return Result;
+}
 
 // Usage: multNoShare a1 a2 b2
 int main(int argc, char* argv[]){
